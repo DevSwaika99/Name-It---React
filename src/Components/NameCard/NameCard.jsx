ad0bf202a0b39c8508607097c02a8fd934ba9553
DevSwaika99 <66863974+DevSwaika99@users.noreply.github.com>
2021-06-04 14:40:52 +0530

    Added finished project

---
+++ src/Components/NameCard/NameCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./NameCard.css"
+
+const nameCheapURL = "https://www.namecheap.com/domains/registration/results/?domain=";
+
+const NameCard = ({ suggestedName }) => {
+    return (
+        <a href={`${nameCheapURL}${suggestedName}`} 
+            className="card-link" 
+            target="_blank"
+            rel="noreferrer">
+                <div className="result-name-card">
+                    <p className="result-name">{suggestedName}</p>
+                </div>
+        </a>
+    )
+}
+
+export default NameCard
---
+++ src/Components/ResultsContainer/ResultsContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import NameCard from '../NameCard/NameCard'
+import "./ResultsContainer.css"
+
+const ResultsContainer = ({ suggestedNames }) => {
+
+    const suggestName = suggestedNames.map(suggestedName => {
+        return <NameCard 
+                    key={suggestedName} 
+                    suggestedName={suggestedName}
+                />
+    })
+
+    return (
+        <div className="results-container">{suggestName}</div>
+    )
+}
+
+export default ResultsContainer
